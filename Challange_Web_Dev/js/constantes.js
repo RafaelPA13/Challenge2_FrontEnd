f0070558f9e34ecf0e33fc22a0983dfdca15f0f7
RafaelPA13 <rafaporann@gmail.com>
2024-09-09 18:38:28 -0300

    Adição do menu responsivo

--- Challange_Web_Dev/js/constantes.js
+++ Challange_Web_Dev/js/constantes.js
@@ -152,3 +152,13 @@ const PILOTOS = [
     foto: '../img/Pilotos/Maximilian-Gunther.png',
   },
 ];
+
+
+const btnMobile = document.getElementById('btn-mobile')
+
+function alternarMenu(){
+    const nav = document.getElementById('nav')
+    nav.classList.toggle('aberto')
+}
+
+btnMobile.addEventListener('click', alternarMenu)
