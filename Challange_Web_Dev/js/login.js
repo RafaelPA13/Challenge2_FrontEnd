228c3a458d9c0b39df4b605de6195e544b464f4d
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-17 22:20:31 -0300

    fix login

--- Challange_Web_Dev/js/login.js
+++ Challange_Web_Dev/js/login.js
@@ -1,10 +1,13 @@
-document.querySelector('form').addEventListener('submit', function(e) {
+document.querySelector('#loginForm').addEventListener('submit', function (e) {
   const nome = document.querySelector('input[name="nome"]').value;
   const email = document.querySelector('input[name="email"]').value;
   const senha = document.querySelector('input[name="senha"]').value;
-
+  e.preventDefault();
   if (!nome || !email || !senha) {
-      alert('Por favor, preencha todos os campos.');
-      e.preventDefault(); // Prevent form submission
+    alert('Por favor, preencha todos os campos.');
+
+    //se a validação for bem sucedida, redirecione para a home 
+  } else {
+    window.location.href = "./home.html";
   }
 });
